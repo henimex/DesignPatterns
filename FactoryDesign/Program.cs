578d4fd8664e0ff32e577f8cfdbfbd6f3216944f
henimex <49072861+henimex@users.noreply.github.com>
2021-02-02 00:08:30 +0300

    Add project files.

---
+++ FactoryDesign/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryDesign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LoggerFactory.CustomerManager cm = new LoggerFactory.CustomerManager();
+            cm.Save();
+
+            Console.ReadLine();
+        }
+    }
+
+    public class LoggerFactory : LoggerFactory.ILoggerFactory
+    {
+
+
+        public ILogger CreateLogger()
+        {
+            //Business to decide factory
+            return new EdLogger();
+        }
+
+        public interface ILogger
+        {
+            void Log();
+        }
+
+        public interface ILoggerFactory
+        {
+
+        }
+
+        public class EdLogger : ILogger
+        {
+            public void Log()
+            {
+                Console.WriteLine("Logged With EdLogger");
+            }
+        }
+
+        public class CustomerManager
+        {
+            public void Save()
+            {
+                Console.WriteLine("Saved");
+                ILogger logger = new LoggerFactory().CreateLogger();
+                logger.Log();
+            }
+        }
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Bu olay ninjectin acılımı işleyişi
+
+
+            var customerManager = CustomerManager.CreateAsSingleton();
+
+
+            Console.WriteLine(customerManager.DoSomething());
+            customerManager.Add();
+            Console.ReadLine();
+        }
+    }
+
+    class CustomerManager
+    {
+        private static CustomerManager _customerManager;
+        private static object _lockObject = new object();
+
+        private CustomerManager()
+        {
+
+        }
+
+        public static CustomerManager CreateAsSingleton()
+        {
+            //return _customerManager ?? (_customerManager = new CustomerManager());
+            lock (_lockObject)
+            {
+                if (_customerManager == null)
+                {
+                    _customerManager = new CustomerManager();
+                }
+            }
+            return _customerManager;
+        }
+
+        public int DoSomething()
+        {
+            return 3;
+        }
+
+        public void Add()
+        {
+            Console.WriteLine("Added");
+        }
+    }
+}
